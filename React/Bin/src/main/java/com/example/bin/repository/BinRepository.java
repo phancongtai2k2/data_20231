7dbf796b7a87c354da71663868c1adfe4a0870eb
Tài Công Phan <51170161+phancongtai2k2@users.noreply.github.com>
2025-02-08 14:59:57 +0700

    Delete React directory

--- React/Bin/src/main/java/com/example/bin/repository/BinRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.bin.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.bin.Bin;
-
-public interface BinRepository extends JpaRepository<Bin, Integer> {
-}
--- React/Bin/src/main/java/com/example/bin/repository/HistoryRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.bin.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.bin.History;
-
-public interface HistoryRepository extends JpaRepository<History, Integer> {
-}
